1bbbbac4b56d10ce7cd6bc7ae75906dae463d5ab
golshandvr-del <golshandvr-del@users.noreply.github.com>
2026-09-02 15:01:43 +0000

    chore: link favicon in page head

--- src/index.tsx
+++ src/index.tsx
@@ -181,6 +181,7 @@ app.get('/', (c) => {
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
 <meta name="theme-color" content="#0b0e11">
 <title>XAUUSD | قیمت لحظه‌ای طلا</title>
+<link rel="icon" type="image/svg+xml" href="/static/favicon.svg">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;700;800&display=swap" rel="stylesheet">
